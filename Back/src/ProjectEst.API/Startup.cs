9afe9ba5da832c3c12afb7b1b9ef593ab1f2471c
Felipe Mello <felipe.mello@fbest.org.br>
2022-07-21 09:12:48 -0300

    Part 4 Angular

--- Back/src/ProjectEst.API/Startup.cs
+++ Back/src/ProjectEst.API/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ProjectEst.API.Data;
 
 namespace ProjectEst.API
 {
@@ -24,15 +26,22 @@ namespace ProjectEst.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
 
+
+        public void ConfigureServices(IServiceCollection services)
+{
+        services.AddDbContext<DataContext>(
+            context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+                );    
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjectEst.API", Version = "v1" });
             });
-        }
+    services.AddControllers();
+
+
+}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
